cf88d11967cf039194edbd097be9e5d558ee0031
edu-ministori <edu.ministori@gmail.com>
2023-12-04 16:46:33 +0900

    code

---
+++ AvoidGame/AvoidGame/Game.cpp
@@ -0,0 +1,65 @@
+#include "Game.h"
+
+Game::Game()
+{
+	// 윈도우 초기화 및 생성
+	this->InitWindowPointer();
+	this->InitWindow();
+}
+
+Game::~Game()
+{
+	this->DeleteWindow();
+}
+
+void Game::InitWindowPointer()
+{
+	// 포인터 변수 초기화
+	this->Window = nullptr;
+}
+
+void Game::InitWindow()
+{
+	// 윈도우 동적 생성
+	this->Window = new RenderWindow(VideoMode(1280, 720), "Avoid Game", Style::Titlebar | Style::Close);
+}
+
+void Game::DeleteWindow()
+{
+	// 윈도우 메모리에서 제거
+	delete this->Window;
+}
+
+const bool Game::Running() const
+{
+	return this->Window->isOpen();
+}
+
+void Game::EventHandler()
+{
+	// 이벤트 발생 감지
+	while (this->Window->pollEvent(Event)) {
+
+		// 이벤트 종류 판단
+		switch (Event.type) {
+
+		case Event::Closed:
+			// 동작
+			this->Window->close();
+			break;
+		case Event::KeyPressed:
+			if (Event.key.code == Keyboard::Escape) this->Window->close();
+			break;
+		}
+
+	}
+}
+
+void Game::Render()
+{
+	// Clear
+	this->Window->clear(Color(50, 50, 50, 255));
+
+	// Display
+	this->Window->display();
+}
---
+++ AvoidGame/AvoidGame/Game.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+using namespace sf;
+
+class Game
+{
+private:
+	RenderWindow* Window;
+	Event Event;
+
+public:
+	Game();
+	~Game();
+
+	void InitWindowPointer();
+	void InitWindow();
+	void DeleteWindow();
+
+	const bool Running() const;
+	void EventHandler();
+
+	void Render();
+};
+
